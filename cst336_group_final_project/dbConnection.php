f1d978b09ba2723b517c391bd36b987ad317b16e
Grace Alvarez <graalvarez@csumb.edu>
2018-08-08 05:28:49 +0000

    Commit Group Final

--- cst336_group_final_project/dbConnection.php
+++ cst336_group_final_project/dbConnection.php
@@ -12,21 +12,21 @@
         $username = "root";
         $password = "";
     
-        //when connecting from Heroku
-        if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-            $host = "localhost";
-            $username = "b6ef724ec50a2b";
-            $password = "8db34bf9";
+//when connecting from Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+
+    //creates db connection
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     
-            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-            $host = $url["host"];
-            $dbname = substr($url["path"], 1);
-            $username = $url["user"];
-            $password = $url["pass"];
-        } 
+    //display errors when accessing tables
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-        return $dbConn;
+    return $dbConn;
     }
 ?>
