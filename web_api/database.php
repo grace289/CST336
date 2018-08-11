3098587b7a45612dad9b4c3487241f3ca5331a23
Grace Alvarez <graalvarez@csumb.edu>
2018-08-11 06:23:39 +0000

    Extra Credit

--- web_api/database.php
+++ web_api/database.php
@@ -1,26 +1,25 @@
-    <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "ebay";
-
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
-	        //when connecting from Heroku
-        if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-            $host = "localhost";
-            $username = "b6ef724ec50a2b";
-            $password = "8db34bf9";
+<?php
+function getDatabaseConnection($dbname = 'ebay'){
+    $host = 'localhost'; //cloud9
+    //$dbname = 'tcp';
+    $username = 'root';
+    $password = '';
     
-            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-            $host = $url["host"];
-            $dbname = substr($url["path"], 1);
-            $username = $url["user"];
-            $password = $url["pass"];
-        } 
+//when connecting from Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+
+    //creates db connection
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+    //display errors when accessing tables
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    return $dbConn;
+}    
 ?>
